16475dcffb7e7547c6562b959dca9ae1949c2c45
Reuven Lerner <reuven@lerner.co.il>
2024-07-23 13:00:09 +0300

    gitautosync update 94

---
+++ mysum.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def mysum(numbers):
+    total = 0
+
+    for one_number in numbers:
+        total += one_number
+
+    return total
+
+print(mysum([10, 20, 30, 40, 50]))
+print(mysum([10, 20, 30, 'a', 50]))
