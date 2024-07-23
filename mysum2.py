a27669048a0eea3ab58a2bf32a56c6637335f7d0
Reuven Lerner <reuven@lerner.co.il>
2024-07-23 13:08:06 +0300

    gitautosync update 114

---
+++ mysum2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from typing import Sequence, Union
+
+def mysum(numbers:Sequence[float]| set[float]) -> float:
+    total : float = 0
+
+    for one_number in numbers:
+        total += one_number
+
+    return total
+
+print(mysum([10, 20, 30, 40, 50]))
+print(mysum([10, 20, 30, 40.0, 50]))
+print(mysum((10, 20, 30, 40.0, 50)))
+print(mysum({10, 20, 30, 40.0, 50}))
