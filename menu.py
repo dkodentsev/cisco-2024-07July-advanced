d609617e82c2bf8e6d65a9dfd9b0a03bda8f2cf6
Reuven Lerner <reuven@lerner.co.il>
2024-07-24 10:58:43 +0300

    gitautosync update 49

--- menu.py
+++ menu.py
@@ -6,3 +6,7 @@ def menu(*args):
             return s
 
         print(f'{s} is not a valid option; try again')
+
+if __name__ == '__main__':
+    choice = menu('x', 'y', 'z')
+    print(f'User chose {choice}')
