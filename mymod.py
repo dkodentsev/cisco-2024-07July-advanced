8006d01ec5af1966be851de23c74be5a4b6dda98
Reuven Lerner <reuven@lerner.co.il>
2024-07-24 10:31:25 +0300

    gitautosync update 34

--- mymod.py
+++ mymod.py
@@ -7,4 +7,7 @@ y = [10, 20, 30]
 def hello(name):
     return f'Hello, {name}!'
 
-print(f'Goodbye from {__name__}')
+
+# only print goodbye if the program was run, rather than imported
+if __name__ == '__main__':
+    print(f'Goodbye from {__name__}')
