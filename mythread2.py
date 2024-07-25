338ee956b9d7ebcd4801b0ce3ad9e15427377379
Reuven Lerner <reuven@lerner.co.il>
2024-07-25 15:57:28 +0300

    gitautosync update 44

--- mythread2.py
+++ mythread2.py
@@ -14,10 +14,17 @@ def hello(n):
     time.sleep(random.randint(0, 3))
     print(f'[{n}] Hello!')
 
+all_threads = []
+
 for i in range(10):
     # create a new Thread object, telling it what function to run
     # whatever arguments we want to pass are in the "args" kwargs
     t = threading.Thread(target=hello, args=(i,))
     t.start()
+    all_threads.append(t)
+
+# join them together, meaning: wait for all of them to finish
+for one_thread in all_threads:
+    one_thread.join()   # this blocks
 
 print('Done!')
