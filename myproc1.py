224a515bc6e2820e410a18f13c1862340d14bec4
Reuven Lerner <reuven@lerner.co.il>
2024-07-25 16:31:46 +0300

    gitautosync update 94

---
+++ myproc1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# if we want, we can create a ThreadPoolExecutor
+# this creates a pool of threads that can be reused for our functions
+# we can then submit our functions to the executor
+# it'll return a "future object," which will (eventually) have the result
+
+# the style of using ThreadPoolExecutor is more elegant and easier to understand
+
+from concurrent.futures import ThreadPoolExecutor, wait
+
+def square(n):
+    return n ** 2
+
+with ThreadPoolExecutor(max_workers=5) as executor:
+    all_results = []
+
+    for i in range(10):
+        result = executor.submit(square, i)  # 1st arg is function, rest are args to it
+        all_results.append(result)
+
+    # wait returns two sets, typically called done and not_done
+    # by default, wait will return its results only when all threads are done
+    done, not_done = wait(all_results)
+
+    # done, not_done = wait(all_results, return_when=FIRST_COMPLETED)
+
+    for one_item in done:
+        print(one_item.result())
