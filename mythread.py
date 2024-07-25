4baa8b89be7c1c9d6f8cc2cd45dfd4f841463f0f
Reuven Lerner <reuven@lerner.co.il>
2024-07-25 15:54:21 +0300

    gitautosync update 41

--- mythread.py
+++ mythread.py
@@ -8,7 +8,14 @@
 
 import time
 import random
+import threading
 
 def hello(n):
     time.sleep(random.randint(0, 3))
     print(f'[{n}] Hello!')
+
+for i in range(10):
+    # create a new Thread object, telling it what function to run
+    # whatever arguments we want to pass are in the "args" kwargs
+    t = threading.Thread(target=hello, args=(i,))
+    t.start()
