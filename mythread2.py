a83bb366fa24eeedcd915ba184fba58ec8bf9e77
Reuven Lerner <reuven@lerner.co.il>
2024-07-25 15:56:26 +0300

    gitautosync update 43

---
+++ mythread2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# I want to run a function numerous times concurrently with itself.
+# I can do that with threads:
+# - I define the function
+# - I start up a bunch of threads, telling them to execute that function
+# - I tell the threads to run
+
+import time
+import random
+import threading
+
+def hello(n):
+    time.sleep(random.randint(0, 3))
+    print(f'[{n}] Hello!')
+
+for i in range(10):
+    # create a new Thread object, telling it what function to run
+    # whatever arguments we want to pass are in the "args" kwargs
+    t = threading.Thread(target=hello, args=(i,))
+    t.start()
+
+print('Done!')
