e8a94bea106d8870289e84c77e61083699339e97
Reuven Lerner <reuven@lerner.co.il>
2024-07-25 16:24:21 +0300

    gitautosync update 82

--- mythread7.py
+++ mythread7.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+
+# if we want, we can create a ThreadPoolExecutor
+# this creates a pool of threads that can be reused for our functions
+# we can then submit our functions to the executor
+# it'll return a "future object," which will (eventually) have the result
+
+# the style of using ThreadPoolExecutor is more elegant and easier to understand
